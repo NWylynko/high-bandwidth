a6e3e769dd270f465c3b27e0504cb2aa4f5393cb
Nick Wylynko <nick1014375@gmail.com>
2022-02-18 21:28:15 +0800

    Add cache devalidating

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,14 +2,16 @@ import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const { nextUrl, method } = req;
+
   // Store the response so we can modify its headers
   const response = NextResponse.next();
 
-  // Set custom header
-  response.headers.set(
-    "Cache-Control",
-    "public, max-age=43200, s-maxage=86400, stale-while-revalidate=129600"
-  );
+  // only cache get requests
+  if (method === "GET") {
+    // we can cache for a year because by calling 'firebase deploy' it will de-validate the cache
+    response.headers.set("Cache-Control", "public, max-age=31536000");
+  }
 
   // Return response
   return response;
