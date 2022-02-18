cba5241e07fe54a6bb348de6d1022ecc98510ba1
Nick Wylynko <nick1014375@gmail.com>
2022-02-18 22:44:47 +0800

    Add unit testing

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import App from "../pages/_app";
+import HomePage from "../pages/index";
+
+it("renders homepage unchanged", () => {
+  const { container } = render(<App Component={HomePage} pageProps={{}} />);
+  expect(container).toMatchSnapshot();
+});
